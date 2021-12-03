0cf895a679b272af0409b32f836d8288be171982
gael <gaelzarco@Mac-mini.local>
2021-12-02 18:34:26 -0800

    could not get submit button to work

--- public/admin.js
+++ public/admin.js
@@ -1,2 +1,39 @@
+async function main() {
 
-// Your Code Here
+    let response = await fetch('http://localhost:3001/listBooks')
+    let books = await response.json()
+
+    console.log(books)
+    books.forEach(renderBookUI)
+}
+
+function renderBookUI (book){
+    let bookListUI = document.getElementById('root')
+    bookListUI.innerHTML += `
+            <label for='${book.quantity}'>${book.title}</label>
+            <input type='text' id='book${book.id}' name='${book.quantity}' value='${book.quantity}'>
+            <input type='submit' value='Sumbit'>
+        `
+}
+
+async function updateQuantity (id, quantity) {
+    let response = await fetch('http://localhost:3001/updateBook', {
+        method: 'PATCH',
+        headers: {'Content-Type': 'application/json'},
+        body: JSON.stringify({'id':id, 'quantity':quantity,})
+    ,})
+}
+
+
+main()
+
+// async function maiin() {
+//     let response = await fetch('http://localhost:3001/updateBook', {
+//         method: 'PATCH', 
+//         headers: {'Content-Type': 'application/json'}, 
+//         body: JSON.stringify({ 'id':3, 'title':'Legends of Arathrae',}),});
+//             let updatedBook = await response.json();
+//             console.log(updatedBook)
+// }
+        
+//         maiin()
